479777050a93eaae8111429baec81bcaf4472709
Ulises Santiago Caceres Licona <37051136+ulisesSan@users.noreply.github.com>
2025-10-26 14:56:42 -0600

    Se inicializa proyecto

---
+++ src/main/java/com/telcel/mpp/MppManipuladotApplication.java
@@ -0,0 +1,13 @@
+package com.telcel.mpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MppManipuladotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MppManipuladotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telcel/mpp/MppManipuladotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telcel.mpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MppManipuladotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
